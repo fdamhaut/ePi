549a2ad8a1ab84f1af13d3f2fc8757bdc4a0a7a3
Florian D <36177525+fdamhaut@users.noreply.github.com>
2019-10-31 16:55:20 +0100

    Update module.cpp

--- Lib/module.cpp
+++ Lib/module.cpp
@@ -7,8 +7,14 @@ int add(int i, int j) {
     return i + j;
 }
 
+int retsum56(int k) {
+	return k+56;
+}
+
 PYBIND11_MODULE(addition, m) {
     m.doc() = "pybind11 addition"; // optional module docstring
 
     m.def("add", &add, "A function which adds two numbers");
+	
+	m.def("sum56", &retsum56, "add 56");
 }
