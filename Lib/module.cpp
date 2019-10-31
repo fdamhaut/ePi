e4498cbc718df7023763a65d6905ca89e3f54d79
Florian D <36177525+fdamhaut@users.noreply.github.com>
2019-10-31 16:49:18 +0100

    Update module.cpp

--- Lib/module.cpp
+++ Lib/module.cpp
@@ -1,26 +1,14 @@
-#include <cmath>
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
 
-double fun56() {
-	return 56;
+int add(int i, int j) {
+    return i + j;
 }
 
-double funadd56(double x) {
-	return x + 56;
-}
+PYBIND11_MODULE(addition, m) {
+    m.doc() = "pybind11 addition"; // optional module docstring
 
-
-PYBIND11_MODULE(test56, m) {
-	m.def("fun56", &fun56, R"pbdoc(
-		return 56.
-    )pbdoc");
-
-#ifdef VERSION_INFO
-	m.attr("__version__") = VERSION_INFO;
-#else
-	m.attr("__version__") = "dev";
-#endif
+    m.def("add", &add, "A function which adds two numbers");
 }
